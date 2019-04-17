7e81c4fed035808e51e10701449bb57f332012a0
Ian Logan <itlogan9@gmail.com>
2019-04-17 12:07:32 -0500

    Add props to comment card

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import faker from "faker";
 
 import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Jim"
+        timeAgo="Today at 4:45PM"
+        content="Nice!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jacob"
+        timeAgo="Today at 2:45AM"
+        content="Super cool, man"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jared"
+        timeAgo="Yesterday at 11:45PM"
+        content="ugh..."
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
