502c7938f165d4d5993d66b1969a3162cf48c1d9
Ian Logan <itlogan9@gmail.com>
2019-04-18 17:46:24 -0500

    Refactor App into stateful component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-  return <div>Hello World!</div>;
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: null,
+      long: null
+    };
+  }
+
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+        <div>Longitude: {this.state.long}</div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
