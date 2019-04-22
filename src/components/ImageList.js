08104eb5b263c054e8128c727c3e1e1cc6af4144
Ian Logan <itlogan9@gmail.com>
2019-04-22 18:41:54 -0500

    Add unique identifiers to images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const ImageList = props => {
   console.log(props.images);
-  const imageDivs = props.images.map(image => <img src={image.urls.regular} />);
+  const imageDivs = props.images.map(({ description, id, urls }) => (
+    <img key={id} src={urls.regular} alt={description} />
+  ));
   return <div>{imageDivs}</div>;
 };
 
