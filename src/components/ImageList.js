9b4f885cd0b61beb1d8ad78d7f5223c79d8e43e2
Ian Logan <itlogan9@gmail.com>
2019-04-22 19:33:55 -0500

    Add basic grid styling to images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ImageList.css";
 
 const ImageList = props => {
   console.log(props.images);
   const imageDivs = props.images.map(({ description, id, urls }) => (
     <img key={id} src={urls.regular} alt={description} />
   ));
-  return <div>{imageDivs}</div>;
+  return <div className="image-list">{imageDivs}</div>;
 };
 
 export default ImageList;
