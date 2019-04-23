5aed5d034256bb325cc2f2e8e5a302dbe7914593
Ian Logan <itlogan9@gmail.com>
2019-04-22 19:47:25 -0500

    Pass image info to new ImageCard component

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./ImageList.css";
 
+import ImageCard from "./ImageCard";
+
 const ImageList = props => {
   console.log(props.images);
-  const imageDivs = props.images.map(({ description, id, urls }) => (
-    <img key={id} src={urls.regular} alt={description} />
+  const imageDivs = props.images.map(image => (
+    <ImageCard key={image.id} image={image} />
   ));
   return <div className="image-list">{imageDivs}</div>;
 };
