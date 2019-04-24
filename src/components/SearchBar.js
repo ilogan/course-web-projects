3d96d13579edb4c80793edc3d1df9fe2bf60cbdc
Ian Logan <itlogan9@gmail.com>
2019-04-24 09:53:01 -0500

    Add event handling to search bar text input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {
+    term: ""
+  };
+
+  handleChange = event => {
+    this.setState({ term: event.target.value });
+  };
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.handleChange}
+            />
           </div>
         </form>
       </div>
