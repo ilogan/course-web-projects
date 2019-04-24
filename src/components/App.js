e7e1f37fd228e6e735730ce422428d6dff931892
Ian Logan <itlogan9@gmail.com>
2019-04-24 12:02:56 -0500

    Set list of videos as App state

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,25 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  handleTermSubmit = term => {
-    youtube.get("/search", {
+  state = {
+    videos: []
+  };
+
+  handleTermSubmit = async term => {
+    const response = await youtube.get("/search", {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar handleTermSubmit={this.handleTermSubmit} />
+        <SearchBar handleTermSubmit={this.handleTermSubmit} />I have{" "}
+        {this.state.videos.length} videos
       </div>
     );
   }
