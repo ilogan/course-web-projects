5937575a9df6b8c6bc345024c95053debcdfdc16
Ian Logan <itlogan9@gmail.com>
2019-04-24 11:01:55 -0500

    Setup axios scaffolding for youtube api connection

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyA27rBPiDrzN97BxVTbQYJ3kfr - ZuUsQ4U";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
