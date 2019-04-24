ffd8131f4a8a390aa787078c1c25751fa397f54c
Ian Logan <itlogan9@gmail.com>
2019-04-24 10:11:20 -0500

    Prevent submit from refreshing page

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,10 +8,14 @@ class SearchBar extends React.Component {
   handleChange = event => {
     this.setState({ term: event.target.value });
   };
+
+  handleSubmit = event => {
+    event.preventDefault();
+  };
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="field">
             <label>Video Search</label>
             <input
