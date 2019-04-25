154c2942ef488692b31d3385a721f97803326cdf
Ian Logan <itlogan9@gmail.com>
2019-04-25 13:38:30 -0500

    Add selected videos to App state when clicked

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,8 @@ import youtube from "../apis/youtube";
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   };
 
   handleTermSubmit = async term => {
@@ -19,11 +20,18 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  handleVideoSelect = video => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar handleTermSubmit={this.handleTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          handleVideoSelect={this.handleVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
-  const videoDivs = videos.map(video => <VideoItem video={video} />);
+const VideoList = ({ videos, handleVideoSelect }) => {
+  const videoDivs = videos.map(video => (
+    <VideoItem video={video} handleVideoSelect={handleVideoSelect} />
+  ));
   return <div className="ui relaxed divided list">{videoDivs}</div>;
 };
 
