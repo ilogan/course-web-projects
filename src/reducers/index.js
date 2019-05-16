809f587c46f85e780886316924b5d7e4e0331180
Ian Logan <itlogan9@gmail.com>
2019-05-16 11:32:53 -0500

    Create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combinedReducers } from "redux";
+
+// Reducer
+const songsReducer = () => {
+  return [
+    { title: "Smells Like Teen Spirit", duration: "4:05" },
+    { title: "Macerana", duration: "2:30" },
+    { title: "All Star", duration: "1:45" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combinedReducers({
+  songs: songsReducer,
+  selectedSongs: selectedSongReducer
+});
