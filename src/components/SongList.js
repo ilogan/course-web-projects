b4f9ec36f7daa6b485296489ab6f7cff6d747bf8
Ian Logan <itlogan9@gmail.com>
2019-05-16 13:03:32 -0500

    Connect SongList component to redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,18 @@
+// Common theme with connecting react to redux
 import React, { Component } from "react";
-import { connect } from "react-redux";
+import { connect } from "react-redux"; // 1) always import connect
 
 class SongList extends Component {
   render() {
+    // 3) this.props === {songs: state.songs} (i.e. this.props equals mapStateToProps value)
     return <div>SongList</div>;
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+  // 2) always mapStateToProps accepts some state and returning an object
+  return { songs: state.songs };
+};
+
+// 4 pass mapStateToProps to connect and component to second set of parantheses
+export default connect(mapStateToProps)(SongList);
