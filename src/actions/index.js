a0fa3dcbe768381c6e51ceaed71fc4d916b7c2d4
Ian Logan <itlogan9@gmail.com>
2019-06-03 18:18:58 -0500

    Wire editStream action to edit button

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,6 +36,7 @@ export const signOut = () => {
 export const createStream = formValues => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post("/streams", { ...formValues, userId });
+
   dispatch({ type: CREATE_STREAM, payload: response.data });
   // Programmatic navigation to get user back to root route
   history.push("/");
@@ -43,17 +44,21 @@ export const createStream = formValues => async (dispatch, getState) => {
 
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get("/streams");
+
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 export const fetchStream = id => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
+
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
+
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = id => async dispatch => {
