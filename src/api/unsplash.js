4dea1979c38053b73946943da286d54183750db6
Ian Logan <itlogan9@gmail.com>
2019-06-08 14:40:45 -0500

    Prep for github

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,7 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID e7104ddddad3e0bfcf809ff4de7b2439c77acf5025418a297044507a3cefb722"
+      // disabled
+      "new-key-here"
   }
 });
