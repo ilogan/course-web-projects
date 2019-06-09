0f0456f2bfbe8d1326708e45bfa311dff1b017e7
Ian Logan <itlogan9@gmail.com>
2019-06-09 12:31:26 -0500

    Refactor API call

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 
+const PokeAPI =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
 class Pokecard extends Component {
   render() {
     const { id, name, type, base_experience } = this.props;
     return (
       <div>
         <p>{name}</p>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-          alt="name"
-        />
+        <img src={`${PokeAPI}${id}.png`} alt="name" />
         <p>Type: {type}</p>
         <p>EXP: {base_experience}</p>
       </div>
