a283f444e1a5d113d10c05a97abdaa4944d81550
Ian Logan <itlogan9@gmail.com>
2019-06-09 13:45:11 -0500

    Add styling to Pokecards

--- src/components/Pokecard.js
+++ src/components/Pokecard.js
@@ -9,10 +9,18 @@ class Pokecard extends Component {
     const { id, name, type, base_experience } = this.props;
     return (
       <div className="Pokecard">
-        <h1>{name}</h1>
-        <img src={`${PokeAPI}${id}.png`} alt="name" />
-        <div>Type: {type}</div>
-        <div>EXP: {base_experience}</div>
+        <div className="Pokecard-content">
+          <h1 className="Pokecard-header">{name}</h1>
+          <img
+            src={`${PokeAPI}${id}.png`}
+            alt="name"
+            className="Pokecard-img"
+          />
+          <div className="Pokecard-info">
+            <div>Type: {type}</div>
+            <div>EXP: {base_experience}</div>
+          </div>
+        </div>
       </div>
     );
   }
