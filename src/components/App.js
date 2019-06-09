fb08b77317f3d0fa77b3bc44d8bec8bb4303e1b4
Ian Logan <itlogan9@gmail.com>
2019-06-09 12:58:49 -0500

    Add back default CRA styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 
 import Pokedex from "./Pokedex";
 
+import "./App.css";
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Pokedex />
       </div>
     );
