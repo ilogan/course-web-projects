78ded196960dcef58ac9de58458df559cd192ea0
Ian Logan <itlogan9@gmail.com>
2019-06-09 12:08:31 -0500

    Add header to Pokedex

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -6,6 +6,7 @@ class Pokedex extends Component {
   render() {
     return (
       <div>
+        <h1>Pokedex</h1>
         <Pokecard id={4} name="Charmander" type="fire" base_experience={62} />
       </div>
     );
