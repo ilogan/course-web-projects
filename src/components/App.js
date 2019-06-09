c8acf70e573bbec2b8e0b8660214caaf1a664cfa
Ian Logan <itlogan9@gmail.com>
2019-06-09 15:16:45 -0500

    Create basic Pokegame component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Pokedex from "./Pokedex";
+import Pokegame from "./Pokegame";
 
 import "./App.css";
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
