1d3236be851672c319c964f27ecbdee9c6e7a927
Ian Logan <itlogan9@gmail.com>
2019-06-14 14:09:15 -0500

    Render pages

--- routes.js
+++ routes.js
@@ -1,5 +1,52 @@
 const requestHandler = (req, res) => {
-  console.log(console.log(req));
+  res.setHeader("Content-Type", "text/html");
+  const { url, method } = req;
+
+  switch (url) {
+    case "/": {
+      res.write(
+        `<html>
+            <head>
+              <title>Users</title>
+            </head>
+            <body>
+              <h1>Hello there!</h1>
+              <p>This is the main page</p>
+            </body>
+          </html>`
+      );
+      break;
+    }
+    case "/users": {
+      res.write(
+        `<html>
+            <head>
+              <title>Users</title>
+            </head>
+            <body>
+              <ul>
+                <li>Billy</li>
+                <li>Jack</li>
+                <li>Jessica</li>
+              </ul>
+            </body>
+          </html>`
+      );
+      break;
+    }
+    default: {
+      res.write(
+        `<html>
+          <head>
+            <title>Users</title>
+          </head>
+          <body>
+            <h1>Default Page!</h1>
+          </body>
+        </html>`
+      );
+    }
+  }
   res.end();
 };
 
