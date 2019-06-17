f41c0662f6d7d64533ada59f3ec924d17a880639
Ian Logan <itlogan9@gmail.com>
2019-06-17 11:52:59 -0500

    Add middleware

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware 2");
+  res.send("<h1>Task 2</h1>");
+});
+
 /* equiv to:
 const server = http.createServer(app)
 server.listen()
