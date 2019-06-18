b1cc3ca9a8bb4c4c2499345751a403196ff821e8
Ian Logan <itlogan9@gmail.com>
2019-06-18 11:21:49 -0500

    Allow access to static files

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const app = express();
 const port = 3000;
 
 // setup parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false })); // false: no nested objects true: nested objects
+app.use(express.json()); // parses incoming json requests
+app.use(express.urlencoded({ extended: false })); // false: no nested objects true: nested objects | parses form data
+app.use(express.static(path.join(__dirname, "public"))); // allows access to static files in public dir
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
