c22a4754c7cc3b85b86ee53e546a04b8b4fd3f88
Ian Logan <itlogan9@gmail.com>
2019-06-20 20:04:50 -0500

    Add new styling

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,12 @@ const router = express.Router();
 
 // get uses exact path matching
 router.get("/", (req, res) => {
+  const products = adminData.products;
   console.log(adminData.products);
-  res.render("shop");
+  res.render("shop", {
+    products,
+    docTitle: "Shop"
+  });
 });
 
 module.exports = router;
